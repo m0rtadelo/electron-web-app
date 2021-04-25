8abbea3b8579650c3dbd7892b271322365d49529
Ricard Fíguls Mateu <ricard.figuls@gmail.com>
2021-04-25 22:45:49 +0200

    Add modules

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,32 +1,27 @@
 import '../assets/css/bootstrap.min.css';
-import { UI } from './utils/ui';
-const ui = new UI();
-interface IResponse {
-  status: number;
-  data?: any;
-  error?: any;
-}
+import { get, getFormData, addEventListener } from './utils/ui';
+import { IResponse } from './interfaces/response.interface';
 
-window.onload = async function() {
-    ui.get('loginForm').addEventListener('submit',async (event: any) => { event.preventDefault(); void await onSubmit() });
+window.onload = function() {
+    addEventListener('loginForm', 'submit', onSubmit);
 };
 
-function showError({status, error, data}: IResponse) {
+function showResult({status, error, data}: IResponse) {
     const show = status !== 200;
-    ui.get('error-div').style =  show ? 'margin-top: 1em;' : 'display: none;'
-    ui.get('status').innerHTML = status;
-    ui.get('error').innerHTML = status === 401 ? 'Invalid Credentials' : error || data?.error || 'Unknown error';
+    get('error-div').style =  show ? 'margin-top: 1em;' : 'display: none;'
+    get('status').innerHTML = status;
+    get('error').innerHTML = status === 401 ? 'Invalid Credentials' : error || data?.error || 'Unknown error';
     if (!show) {
-        ui.get("login").style = "display:none;"
-        ui.get("app").style = ""
+        get("login").style = "display:none;"
+        get("app").style = ""
     }
 }
 
 const onSubmit = async () => {
-    const data = ui.getFormData();
+    const data = getFormData();
     const result = await (window as any).api.post({
         action: 'login',
         data
     });
-    showError(result);
+    showResult(result);
 }
--- client/src/utils/ui.ts
+++ client/src/utils/ui.ts
@@ -1,15 +1,24 @@
-export class UI {
-    public get = (id: any): any => document.getElementById(id);
-    public getFormData = (): any => {
-        const inputFields = document.getElementsByTagName('input')
-        let data = {}
-        for(let i=0; i<inputFields.length; i++) {
-            const id = inputFields[i].id;
-            const htmlItem = this.get(id);
-            if (htmlItem) {
-                data = { ...data, [id]: htmlItem.value };
-            }
-        }  
-        return data;  
+export const get = (id: any): any => document.getElementById(id);
+
+export const getFormData = (): any => {
+    const inputFields = document.getElementsByTagName('input')
+    let data = {}
+    for(let i=0; i<inputFields.length; i++) {
+        const id = inputFields[i].id;
+        const htmlItem = get(id);
+        if (htmlItem) {
+            data = { ...data, [id]: htmlItem.value };
+        }
+    }  
+    return data;  
+}
+
+export const addEventListener = (id: string, event: string, action: Function) => {
+    const element = get(id);
+    if (element) {
+        element.addEventListener(event, async (event: any) => {
+            event?.preventDefault();
+            return await action();
+        })
     }
 }
