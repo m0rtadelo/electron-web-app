ad8a47ef73ece838b8941f04b524fc6f00425f22
Ricard Fíguls Mateu <ricard.figuls@altran.com>
2021-04-28 14:33:35 +0200

    Fix async actions

--- server/src/actions/login.js
+++ server/src/actions/login.js
@@ -1,6 +1,13 @@
 module.exports.login = function({user, pass}) {
     if (user === 'user' && pass === 'pass') {
-        return { status: 200, data: { user: 'user', id: 1 } };
+      return { status: 200, data: { user: 'user', id: 1 } };
     }
-    return { status: 401, data: { login: false }};
+    if (user === 'admin' && pass === 'pass') {
+      return { status: 200, data: { user: 'admin', id: 2 } };
+  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res({ status: 401, data: { login: false }});
+    }, 3000);
+  })
 }
