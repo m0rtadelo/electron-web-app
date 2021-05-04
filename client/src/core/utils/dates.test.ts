a3a61145e39c8eeb7e74d887f9b71c5645c45787
Ricard Fíguls Mateu <ricard.figuls@gmail.com>
2021-05-04 19:40:54 +0200

    Add tests

--- client/src/core/utils/dates.test.ts
+++ client/src/core/utils/dates.test.ts
@@ -1,7 +1,22 @@
-import getDateString from './dates';
+import { getDateString, getTimeString, getToday } from './dates';
+import { expect } from 'chai';
 
-describe('test', () => {
-  it('comment', () => {
-    expect(true).equal(true);
+describe('Dates', () => {
+  it('should return expected DateString', () => {
+    const date = getDateString(new Date());
+    expect(date.substr(2, 1)).equal('-');
+    expect(date.substr(5, 1)).equal('-');
+    expect(date.length).equal(10);
+  });
+
+  it('should return expected TimeString', () => {
+    const date = getTimeString(new Date());
+    expect(date.substr(2, 1)).equal(':');
+    expect(date.substr(5, 1)).equal(':');
+    expect(date.length).equal(8);
+  });
+
+  it('should return Date object', () => {
+    expect(getToday() instanceof Date).equal(true);
   });
 });
