80194b2bdb3c90462bc90ef81a23d6b2cb280e76
Ricard Fíguls Mateu <ricard.figuls@altran.com>
2021-05-17 20:27:54 +0200

    Add edit contact

--- server/src/actions/patch/contacts.ts
+++ server/src/actions/patch/contacts.ts
@@ -1,6 +1,14 @@
 import { contacts as model } from '../../model/contacts.model';
 
 export const contacts = function(data: any) {
+  const keys = Object.keys(data);
   const contact = model.find((contact) => contact.id === data.id);
-  return contact ? { status: 200, data: contact } : { status: 404, data: { error: 'Contact not found!?' } };
+  if (contact) {
+    keys.forEach((key) => {
+      contact[key] = data[key];
+    });
+    return { status: 200, data: contact };
+  }
+
+  return { status: 404, data: { error: 'Contact not found!?' } };
 };
