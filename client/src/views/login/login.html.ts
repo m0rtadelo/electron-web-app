04ae8ac149554df5d63d512ebca53c11ca895729
m0rtadelo <ricard.figuls@gmail.com>
2021-09-24 22:30:16 +0200

    Add autologin

--- client/src/views/login/login.html.ts
+++ client/src/views/login/login.html.ts
@@ -1,15 +1,7 @@
 export const LOGIN_HTML = `
 <lang-picker></lang-picker>
-<div class="container-sm" style="margin-top: 10em;" id="login">
-  <div class="row justify-content-md-center">
-    <div class="col col-sm-4">
-      <login></login>
-      <app-type id="change" color="grey"></app-type>
-      <banner-error></banner-error>
-      <date-hour color="grey" click="console.log('view click event', this)"
-       style="position: absolute; width: 100%; left: 0px; bottom: 0px;">
-      </date-hour>
-    </div>
-  </div>
-</div>
+<form>
+<input type="hidden" id="user" value="admin">
+<input type="hidden" id="pass" value="pass">
+</form>
 `;
--- client/src/views/login/login.view.ts
+++ client/src/views/login/login.view.ts
@@ -1,27 +1,23 @@
-import { get, getFormData } from '../../core/utils/ui';
+import { getFormData } from '../../core/utils/ui';
 import { LoginService } from './login.service';
 import { LOGIN_HTML } from './login.html';
 import { HomeView } from '../home/home.view';
 import { View } from '../../core';
-import { BannerErrorComponent, LoginComponent, AppTypeComponent } from '../../components';
-import { DateHourComponent } from '../../components/date-hour/date-hour.component';
-import { LangpickerComponent } from '../../components/lang-picker/lang-picker.component';
+import { LangpickerComponent } from '../../components';
 
 export class LoginView extends View {
-  public testData = 'surprise';
   constructor(data?: any) {
     super(LOGIN_HTML, [
-      new BannerErrorComponent(),
-      new LoginComponent(),
-      new DateHourComponent(),
-      new AppTypeComponent(),
       new LangpickerComponent(),
     ], data, new LoginService());
     this.notifyClear();
   }
 
   public onReady() {
-    get('user').focus();
+    // get('user').focus();
+    setTimeout(async () => {
+      this.emmit();
+    }, 0);
   }
 
   public async emmit() {
